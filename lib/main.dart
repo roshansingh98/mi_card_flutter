1bfb184791913799947e749bc2fd3de4d71df820
Roshan Singh <singhroshan98@gmail.com>
2020-08-20 21:16:49 +0530

    Day 2: First try at column and rows and containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
